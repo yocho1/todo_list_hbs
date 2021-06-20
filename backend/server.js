4c006971fbbd17ae7b141ebe82ccdcd47f068aac
yocho1 <achraflachgar456@gmail.com>
2021-06-20 12:59:58 +0000

    connection to the database using mongodb database

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import cors from 'cors'
+import connectDB from './config/db.js'
 dotenv.config()
 
+// database connection
+connectDB()
 
 const app = express()
 
+// middlewares
 app.use(express.json())
+app.use(cors())
 
-app.use('/', (req, res)=>{
-    res.json('todos')
+app.use('/', (req, res) => {
+  res.json('todos')
 })
 
-
+// port
 const PORT = process.env.PORT || 3000
 
+// start our app todos
 app.listen(
   PORT,
   console.log(
