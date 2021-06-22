af11f065756243359efd18119db2517ee7022aa0
yocho1 <achraflachgar456@gmail.com>
2021-06-22 08:36:14 +0000

    add protect routing

--- backend/routes/listRoutes.js
+++ backend/routes/listRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
 const router = express.Router()
 import { addList, getLists, deleteList } from '../controllers/listController.js'
+import { protect } from '../middleware/authMiddleware.js'
 
-router.route('/add').post(addList)
-router.route('/all').get(getLists)
-router.route('/delete/:id').delete(deleteList)
+router.route('/add').post(protect, addList)
+router.route('/all').get(protect, getLists)
+router.route('/delete/:id').delete(protect, deleteList)
 
 export default router
